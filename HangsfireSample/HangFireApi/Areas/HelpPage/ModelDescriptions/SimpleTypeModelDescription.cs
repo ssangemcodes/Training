e586c1536107f9b7e2c3ba1f6757df4024201c78
ssangemcodes <ssangemcodes@gmail.com>
2023-02-26 13:34:44 -0600

    remove unnecessary files

--- HangsfireSample/HangFireApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HangFireApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- HangsfireSample/HangsfireSample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HangsfireSample.MvcApplication" Language="C#" %>
